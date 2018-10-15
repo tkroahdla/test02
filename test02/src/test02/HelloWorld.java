94c35ad72ee07bbede8550dd5130b2d1efec1f48
chosun <chosun@chosun>
2018-10-15 13:40:53 +0900

    The 5-th commit

--- test02/src/test02/HelloWorld.java
+++ test02/src/test02/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from Hotfix");
-		System.out.println("Hello XXXXXX from Hotfix");
+		
+		System.out.println("Hello ZZZZZZ from Hotfix");
 	}
 
 }
