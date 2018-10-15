0fb4a41aef0110aee5961ba19ed252e79fa70d94
chosun <chosun@chosun>
2018-10-15 13:40:12 +0900

    The 4-th commit

--- test02/src/test02/HelloWorld.java
+++ test02/src/test02/HelloWorld.java
@@ -4,9 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
 		System.out.println("Hello from Hotfix");
-
+		System.out.println("Hello XXXXXX from Hotfix");
 	}
 
 }
