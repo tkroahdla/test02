4bd2b3ab280bfa1f16ccc4e71fb372a3477a5e43
chosun <chosun@chosun>
2018-10-17 13:35:51 +0900

    testsetstetststsetset

--- test02/src/test02/HelloWorld.java
+++ test02/src/test02/HelloWorld.java
@@ -3,7 +3,6 @@ package test02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello from Hotfix");
 		
 		System.out.println("Hello ZZZZZZ from Hotfix");
